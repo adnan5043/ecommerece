716c8cbe0bea1d3417131cdeb1483787983ce552
adnan5043 <adnannaeem785@gmail.com>
2023-11-06 18:04:10 +0500

    update signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+          validates :name, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{11}\z/, message: "must be 11 digits" }
+
+attr_accessor :phone  # or attr_accessible :phone, if you're using older Rails versions
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(name: "Adnan Naeem",phone:"03021798035" email: "ad11@example.com", password: "123456")
+
